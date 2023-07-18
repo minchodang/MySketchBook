dcca8efede9340838138ef1630d4405b1ea0d959
minsuKang <minsu910725@gmail.com>
2023-07-18 12:44:58 +0900

    Feat: react-query ssr hydaration 적용

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        unsplash_access_key: process.env.unsplash_access_key,
+        unsplash_secret_key: process.env.unsplash_secret_key,
+    },
+    images: {
+        domains: ['images.unsplash.com'],
+        minimumCacheTTL: 60,
+    },
+    rewrites: () => {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'https://api.unsplash.com/:path*',
+            },
+        ];
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ src/pages/picture/[id].tsx
@@ -0,0 +1,56 @@
+import styled from '@emotion/styled';
+import { QueryClient, dehydrate } from '@tanstack/react-query';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import QueryKeys from '../../../lib/next13-submodule/hooks/Querykey';
+import {
+    fetchPicture,
+    useGetPictureQuery,
+} from '../../../lib/next13-submodule/hooks/useGetPictureQuery';
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const DetailPage = () => {
+    const router = useRouter();
+    const { id } = router.query as { id: string };
+    const { data } = useGetPictureQuery({ id });
+    return (
+        <Container>
+            {data ? (
+                <div key={data?.id}>
+                    <h1>{data?.description || data?.alt_description}</h1>
+                    <Image
+                        src={data?.urls.raw || ''}
+                        height={400}
+                        width={400}
+                        layout="responsive"
+                        placeholder="blur"
+                        alt={data?.description}
+                        blurDataURL={data?.urls.small}
+                    />
+                </div>
+            ) : null}
+        </Container>
+    );
+};
+
+export async function getServerSideProps(context: { query: { id: string } }) {
+    const { id } = context.query;
+
+    const queryClient = new QueryClient();
+    await queryClient.prefetchQuery([QueryKeys.picture], () => {
+        fetchPicture({ id });
+    });
+
+    return {
+        props: {
+            dehydratedState: dehydrate(queryClient),
+        },
+    };
+}
+
+export default DetailPage;
